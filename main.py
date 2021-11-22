95ac6f03fd779d7492246207f0e9af0b8830bdfe
NzanEkok <nzan.patrick@gmail.com>
2021-11-22 22:04:22 +0100

    Added Basic post request

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+from typing import Optional
 from fastapi import FastAPI
+from fastapi.param_functions import Body
+from pydantic import BaseModel 
 
 app = FastAPI()
 
@@ -23,3 +26,25 @@ def months():
     "Nov" : "November",
     "Dec" : "December"
 }
+
+
+                                        #POST method
+
+class post(BaseModel): 
+    id : int
+    title : str
+    content : str
+    published : bool
+    rating : Optional[int]
+
+
+@app.post("/post") #Simple post without validation
+def createpost(payload: dict = Body(...)): 
+    print(payload)
+    return{"New post created" : payload}
+
+
+@app.post("/createpost") #Post with schema validation from pydantic basemodel
+def createpost(Post : post):
+    print(Post.dict())
+    return{"New post created" : Post }
